2959f0207aa90ee47d2c00961594ed7874c28a0e
Manfred Steyer <manfred.steyer@gmx.net>
2022-06-20 11:28:39 +0200

    init

---
+++ apps/my-plugin-e2e/jest.config.ts
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'my-plugin-e2e',
+  preset: '../../jest.preset.ts',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/my-plugin-e2e',
+};
